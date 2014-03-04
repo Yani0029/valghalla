69fd71dd270718d7ac97620533c13c24c0fff898
Thomas Thune Hansen <tth@bellcom.dk>
2014-03-04 07:14:33 +0100

    updated some stuff on the blocks

--- valhalla_blocks/js/valhalla_blocks.js
+++ valhalla_blocks/js/valhalla_blocks.js
@@ -2,7 +2,7 @@ var valhalla_blocks = {
   loadOverview : function(delta){
     jQuery.get("/valhalla_blocks/ajax/view/"+delta, function(data){
       jQuery('#valhalla_block-'+delta).html(data);
-      jQuery('#valhalla_block-'+delta).removeClass('loading-gif');
+      jQuery('#valhalla_block-'+delta).removeClass('spinner-loading');
     });
   }
 };
