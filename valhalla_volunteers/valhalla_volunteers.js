30611356296e5dc4d59ed7e91518aaa9ab14ec19
Thomas Thune Hansen <tth@bellcom.dk>
2014-04-22 10:12:14 +0200

    initial work

--- valhalla_volunteers/valhalla_volunteers.js
+++ valhalla_volunteers/valhalla_volunteers.js
@@ -1,8 +1,88 @@
-/*
- * @copyright bellcom open source aps
- * @author ulrik nielsen <un@bellcom.dk>
- */
+var volunteer_info = volunteer_info || {};
+(function ($) {
+  Drupal.behaviors.valghalla_volunteers = {
+    attach: function (context, settings) {
 
+      $('.js-add-volunteer').on('click', function(){
+        var $parent = $(this).parent();
+        var $el = $(this);
+
+        volunteer_info.post_id = $parent.attr('data-post');
+        volunteer_info.pollingstation_nid = $el.attr('data-pollingstation_nid');
+        volunteer_info.role_nid = $el.attr('data-role_nid');
+        volunteer_info.party_tid = $el.attr('data-party_tid');
+        $('.modal').modal();
+      });
+
+      $('.js-remove-volunteer').live('click', function(){
+        var fcid = $(this).attr('data-fcid');
+        $parent = $(this).parent();
+        $el = $parent.find('.volunteer');
+
+        $parent.find('.edit').remove();
+        $parent.find('.js-remove-volunteer').hide();
+
+        $el.text('...');
+
+        $.post('/ajax/volunteers/station/remove', {'fcid': fcid}, function(data){
+          if(data.success){
+            $el.text('Vælg en deltager.');
+            $parent.find('.js-add-volunteer').show();
+            Drupal.behaviors.valghalla_volunteers.populateTable();
+          }
+        });
+      });
+
+      // Select volunteer from modal
+      $('.js-select-volunteer').live('click', function(event){
+        $('.modal').modal('hide');
+        $el = $('[data-post="'+volunteer_info.post_id+'"]');
+
+        $el.find('.js-add-volunteer').remove();
+        $el.find('div').text('...');
+
+        volunteer_info.volunteer_nid = $(this).attr('data-volunteer_nid');
+
+        $.post('/ajax/volunteers/station/add', volunteer_info, function(data){
+          $el.find('div').html(data.html);
+          $el.append('<a href="/node/'+volunteer_info.volunteer_nid+'/edit?destination=volunteers/station/'+volunteer_info.pollingstation_nid+'" class="btn btn-default btn-xs edit"><span class="glyphicon glyphicon-user"></span></a>');
+
+          $el.append('<a data-fcid="'+data.fcid+'" class="remove btn btn-default btn-xs js-remove-volunteer"><span class="glyphicon glyphicon-minus"></span></a>');
+        });
+
+        setTimeout(function(){
+          Drupal.behaviors.valghalla_volunteers.populateTable();
+        }, 500);
+
+      });
+
+      Drupal.behaviors.valghalla_volunteers.populateTable();
+
+    },
+    populateTable: function(){
+        oTable = $('.volunteers-list').dataTable();
+        oTable.fnClearTable();
+
+        $.get('/ajax/volunteers/station/getvolunteers', function(data){
+          for (var key in data){
+            oTable.fnAddData([ data[key].volunteer_party, data[key].volunteer_name, '<a class="js-select-volunteer" data-volunteer_nid="' + data[key].volunteer_nid + '"><span class="btn btn-xs btn-success">Vælg</span></a>']);
+          }
+          $('.modal .spinner-loading').remove();
+        });
+    },
+    unsetVolunteer: function(nid){
+      var data = Drupal.settings.valghalla_volunteers.volunteers;
+      for (var key in data){
+        if(data[key].volunteer_nid === nid){
+          Drupal.settings.valghalla_volunteers.volunteers.splice(key, 1);
+        }
+      }
+    }
+  };
+})(jQuery);
+
+
+/**
 jQuery(function(){
 
   var $dialog = jQuery("div#dialog");
@@ -87,3 +167,4 @@ jQuery(function(){
       jQuery(this).find('.rsvp-message').hide();
   });
 });
+*/
--- valhalla_volunteers/valhalla_volunteers_to_polling_station.tpl.php
+++ valhalla_volunteers/valhalla_volunteers_to_polling_station.tpl.php
@@ -1,20 +1,24 @@
 <?php if($posts_to_fill): ?>
 <div id="volunteer-station-list">
 <?php foreach($posts_to_fill as $i => $post): ?>
-  
+
   <dl class="clearfix" id="volunteer-station-list-item-<?php print $i ?>">
-    <dt class="label-<?php print $post['title'] ?>"><?php print $post['title'] ?></dt>
-    <dd>
-      <div class="well well-sm col-xs-6"><?php print (isset($existing[$i]) ? $existing[$i]['data'] : t('Vælg en tilforordnet')) ?></div>
-    <a <?php if(isset($existing[$i])): ?> style="display:none;"  <?php endif; ?>
-      href="/ajax/volunteers/station/list/<?php print $party_id ?>/<?php print $station_id ?>/<?php print $i ?>/<?php print $post['nid'] . '-' . $post['party_id'] ?>" class="btn btn-default btn-xs add<?php print (isset($existing[$i]) ? '' : '') ?>"><span class="glyphicon glyphicon-plus"></span></a>
+    <dt class="label-<?php print $post['role_title'] ?>"><?php print $post['role_title'] ?></dt>
+    <dd data-post="<?php print $post['role_nid'] . $post['party_tid'] . $i;?>">
+      <div class="well well-sm col-xs-6"><?php print (isset($existing[$i]) ? $existing[$i]['data'] : t('Vælg en deltager.')) ?></div>
+
+    <a data-role_nid="<?php print $post['role_nid']; ?>"
+       data-party_tid="<?php print $post['party_tid']; ?>"
+       data-pollingstation_nid="<?php print $pollingstation_nid; ?>"
+      <?php if(isset($existing[$i])): ?> style="display:none;"  <?php endif;?> class="btn btn-default btn-xs js-add-volunteer"><span class="glyphicon glyphicon-plus"></span></a>
 
     <?php if(isset($existing[$i])): ?>
       <a href="/node/<?php print $existing[$i]['nid'] ?>/edit?destination=<?php print(implode('/', arg())) ?>" class="btn btn-default btn-xs edit"><span class="glyphicon glyphicon-user"></span></a>
-      <a href="/ajax/volunteers/station/remove/<?php print $existing[$i]['fcid'] ?>/<?php print $i ?>" class="remove btn btn-default btn-xs"><span class="glyphicon glyphicon-minus"></span></a>
+      <a data-fcid="<?php print $existing[$i]['fcid'] ?>" class="remove btn btn-default btn-xs js-remove-volunteer" ><span class="glyphicon glyphicon-minus"></span></a>
     <?php endif; ?>
     </dd>
   </dl>
 <?php endforeach; ?>
 </div>
 <?php endif;?>
+
